878541108d54d99f556c12952a7a44d61947d28b
dadaWilliam <90446055+dadaWilliam@users.noreply.github.com>
2024-09-04 16:10:45 -0400

    modify shape interface, add private, change render func

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,14 +3,19 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    // public Rectangle rectangle;
+    private Shape shape;
+
+    // Renderer(Rectangle rectangle) {
+    // this.rectangle = rectangle;
+    // }
+
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
-    void draw() {
-        double area = rectangle.getArea();
+    public void draw() {
+        double area = shape.getArea();
 
         // assume implementation
 

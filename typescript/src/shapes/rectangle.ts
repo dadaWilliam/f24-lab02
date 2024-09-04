204c580e85272e25d5043abd10fc29093b7fb223
dadaWilliam <90446055+dadaWilliam@users.noreply.github.com>
2024-09-04 16:08:07 -0400

    delete interface Rec, fix render func

--- typescript/src/shapes/rectangle.ts
+++ typescript/src/shapes/rectangle.ts
@@ -1,18 +1,18 @@
-interface Rectangle {
-    width: number,
-    height: number,
-    computeArea: () => number
-}
+// interface Rectangle {
+//     width: number,
+//     height: number,
+//     computeArea: () => number
+//}
 
-function newRectangle(width: number, height: number): Rectangle {
+function newRectangle(width: number, height: number): Shape {
     return {
-        width,
-        height,
+        // width,
+        // height,
 
         computeArea: function (): number {
             return width * height
         }
     }
 }
-
-export { Rectangle, newRectangle }
+export { newRectangle }
+// export { Rectangle, newRectangle }
